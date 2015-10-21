13b82a98aeec9f2e177d9c1d5edf5ecff22c3989
Toba Yusuke <toba@ws.cs.kobe-u.ac.jp>
2015-10-21 15:09:29 +0900

    add design for FW

--- documents/template-design.js
+++ documents/template-design.js
@@ -45,3 +45,73 @@ var xModal = {
 		console.log('xmodal: receive');
 	}
 };
+
+
+///////////////////
+// FW部分
+var FW = {
+	modes: [],
+	subscribers: {
+		stt: []
+	},
+	dataFromModes: [
+		{mode: stt, date:"2015-10-21 14:40:56:12", data:"こんにちは"},
+		{mode: stt, date:"2015-10-21 14:40:56:12", data:"こんにちは"},
+		{mode: stt, date:"2015-10-21 14:40:56:12", data:"こんにちは"}
+	],
+
+	addModal: function(m, config) {
+		//与えられた設定をモードに追加
+		m.config = config;
+		this.modes.push(m);
+
+		// モードごとのviewの作成
+		m.view = this.createNewFrame(m.name);
+
+		// モードごとに初期処理
+		m.init();
+
+		// buttonへのイベント貼り付け
+		$('#' + m.name).find('input[value=run]').click(function() {
+			m.run();
+			m.state = 'running';
+		});
+		$('#' + m.name).find('input[value=stop]').click(function() {
+			m.stop();
+			m.state = '';
+		});
+
+		// streamの設定
+		setInterval(function() {
+			if (m.state == 'running') {
+				m.stream();
+			}
+		}, m.config.streamInterval);
+	},
+
+	//部品作成
+	createNewFrame: function(name, stable_config) {
+		var mainf = $('<div>').attr('class', 'modal');
+		var cbar = $('<div>')
+		.append($('<input>').attr('type', 'button').attr('value', 'run'))
+		.append($('<input>').attr('type', 'button').attr('value', 'stop'))
+		.append($('<input>').attr('type', 'button').attr('value', 'config'));
+		var view = $('<div>').attr('id', name).append(mainf).append(cbar);
+		view.appendTo('#mview');
+		return mainf;
+	},
+
+	register: function(publisher, subscriber){
+		if(subscriber in this.subscribers[publisher])
+			return false;
+		this.subscribers[publisher].push(subscribers);
+	},
+
+	publish: function(modename, data){
+		//save localstrage
+		for (var i in this.subscribers.modename){
+			this.subscribers[i].notify(data);
+		}
+	}
+
+};
